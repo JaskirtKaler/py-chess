3e70ce46675a49d3de54695e492c81283c904fcf
JaskirtKaler <96710929+JaskirtKaler@users.noreply.github.com>
2025-02-11 17:02:50 -0800

    class

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-if __name__ == "__main__":
+class board:
+	def __init__(self, width=8, height=8):
+		self.width = width
+        	self.height = height
+        	self.matrix = [[None for _ in range(width)] for _ in range(height)]
+
+	def place_piece(self, piece, x ,y):
+		pass
+	def display_board(self):
+		pass    
+ __name__ == "__main__":
 	print("Here in main")
